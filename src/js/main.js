8fa3b8cce051366229506d5ae0657a22b3b62ffd
JovensTalentosIFFC <infosisfaperj@gmail.com>
2026-03-03 17:49:00 -0300

    feat: inicialização dos estilos da tela principal

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+const fetchAPI = async () =>{
+
+
+  // como consultar os dados da api
+
+  // const data = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Campos&appid=750472f04629eac0ae572e08097b1438&units=metric&lang=pt_br');
+  // const parsedData = await data.json();
+  // console.log(parsedData)
+}
